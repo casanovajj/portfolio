9e881f943a2063279e769d6b16d920b6ddd4cfb6
casanovajj <91490783+casanovajj@users.noreply.github.com>
2023-10-12 18:22:54 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Janmejay Pathak",
+  title: "Student in Entreprenuer journey",
+  email: "jai247pathak@gmail.com",
+  gitHub: "https://github.com/casanovajj",
+  
+  linkedIn: "Janmejay pathak",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "janmejay1",
+  youTube: "anonymous",
 };
 
 const primaryColor = "#4E567E";
